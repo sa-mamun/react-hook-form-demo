bc274e99fc22adb557e9a320b0e31a65bfc47803
samir <samamun009@gmail.com>
2025-03-05 01:52:56 +0600

    react hook form and devtool integrated

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,21 +1,28 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const YouTubeForm = () => {
+
+    const form = useForm();
+    const { register, control } = form;
+
     return (
         <div>
             <h1>YouTube Form</h1>
 
             <form>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" />
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" {...register("username")} />
 
-            <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" name="email" />
+                <label htmlFor="email">E-mail</label>
+                <input type="email" id="email" {...register("email")} />
 
-            <label htmlFor="channel">Channel</label>
-            <input type="text" id="channel" name="channel" /><br />
+                <label htmlFor="channel">Channel</label>
+                <input type="text" id="channel" {...register("channel")} /><br />
 
-            <button>Submit</button>
+                <button>Submit</button>
             </form>
+            <DevTool control={control} />
         </div>
     );
 };
