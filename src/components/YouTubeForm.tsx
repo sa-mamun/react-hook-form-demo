392754fa68a346596ba92e862c7bf18fb9298cce
sa-mamun <samamun009@gmail.com>
2025-03-05 15:53:22 +0600

    validation, custom validation added

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,24 +1,64 @@
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+type FormValues = {
+    username: string;
+    email: string;
+    channel: string;
+};
+
 const YouTubeForm = () => {
 
-    const form = useForm();
-    const { register, control } = form;
+    const form = useForm<FormValues>();
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
+
+    const onSubmit = (data : FormValues) => {
+        console.log("Form Submitted", data);
+    }
 
     return (
         <div>
             <h1>YouTube Form</h1>
 
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register("username")} />
+                <input type="text" id="username" {...register("username", {
+                    required: {
+                        value: true,
+                        message: 'Username is required'
+                    }
+                })} />
+                <p className="error">{errors.username?.message}</p>
 
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" {...register("email")} />
+                <input type="email" id="email" {...register("email", {
+                    required: 'Email is required',
+                    pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: 'Invalid email address'
+                    },
+                    validate: {
+                        notAdmin: (fieldValue) => {
+                            return (
+                                fieldValue != "admin@example.com" || 
+                                "Enter a different email address"
+                            );
+                        },
+                        notBlackListed: (fieldValue) => {
+                            return (
+                                !fieldValue.endsWith("baddomain.com") || "This domain is not supported"
+                            );
+                        }
+                    }
+                })} />
+                <p className="error">{errors.email?.message}</p>
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel")} /><br />
+                <input type="text" id="channel" {...register("channel", {
+                    required: 'Channel is required'
+                })} />
+                <p className="error">{errors.channel?.message}</p><br />
 
                 <button>Submit</button>
             </form>
